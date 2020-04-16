3ab31d064b3d74240baaecc654cf3d348361f9e7
Reinaldo Neto <reinaldonetof@hotmail.com>
2020-04-15 23:06:21 -0300

    added ejs, index.ejs, publics

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
@@ -6,15 +7,19 @@ const mongo = process.env.MONGODB || "mongodb://localhost/minhas-series";
 const mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
 
-app.use(express.json())
+app.use(express.json());
+app.use(express.static("public"));
 
-app.get("/", (req, res) => res.send("ok"));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => res.render("index"));
 
 mongoose
-  .connect(mongo, {useNewUrlParser: true, useUnifiedTopology: true})
+  .connect(mongo, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
-  app.listen(port, () => console.log("listening on: " + port));
-})
-.catch(e => {
-  console.log(e)
-});
+    app.listen(port, () => console.log("listening on: " + port));
+  })
+  .catch((e) => {
+    console.log(e);
+  });
