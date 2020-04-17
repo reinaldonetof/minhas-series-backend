05ed0d386f484bab1d99ad2a0f3e8604d96ccd79
Reinaldo Neto <reinaldonetof@hotmail.com>
2020-04-16 23:16:13 -0300

    fixed about and home

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => res.render("index"));
+app.get("/sobre", (req, res) => res.render("sobre"));
 
 mongoose
   .connect(mongo, { useNewUrlParser: true, useUnifiedTopology: true })
